7a523a15ac969a805a2eeaacb654a627a84adc5a
cortins-05 <cortins-05@users.noreply.github.com>
2026-05-28 13:27:04 +0200

    revert: remove FFT visualizer (unstable), restore placeholder bars

--- modules/audio-spectrum/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AudioSpectrumModule } from './src/AudioSpectrumModule';
-export type { AudioSpectrumNativeModule, SpectrumData, EventSubscription } from './src/AudioSpectrum.types';
--- modules/audio-spectrum/src/AudioSpectrumModule.ts
+++
@@ -1,5 +0,0 @@
-import { requireNativeModule } from 'expo-modules-core';
-import type { AudioSpectrumNativeModule } from './AudioSpectrum.types';
-
-const AudioSpectrum = requireNativeModule<AudioSpectrumNativeModule>('AudioSpectrum');
-export default AudioSpectrum;
